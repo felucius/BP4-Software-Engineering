e7f45823fee3f3b75ae50fe56ecd7aded2b01464
Maxime de Lange <maximedelange1991@hotmail.com>
2015-05-21 10:10:11 +0100

    Nieuwe klasse Gebruiker aangemaakt

---
+++ Maxime de Lange Individuele opdracht BP4/Maxime de Lange Individuele opdracht BP4/Gebruiker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Maxime_de_Lange_Individuele_opdracht_BP4
+{
+    public class Gebruiker
+    {
+        public Gebruiker()
+        {
+
+        }
+    }
+}
---
+++ Maxime de Lange Individuele opdracht BP4/Maxime de Lange Individuele opdracht BP4/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Maxime_de_Lange_Individuele_opdracht_BP4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Maxime_de_Lange_Individuele_opdracht_BP4")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("daf2af9a-99e8-4940-9122-e643dff642b2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
